30b7f31415cbae2014358dda9ff78531281c6a79
YZ318 <YakovZhuravlev88@gmail.com>
2023-04-22 13:58:30 +0300

    Задача 15

---
+++ home15/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите день недели (1 - понедельник, 2 - вторник и т.д.):");
+int dayOfWeek = int.Parse(Console.ReadLine());
+
+if (dayOfWeek == 6 || dayOfWeek == 7)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
