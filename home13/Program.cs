fdbc97db2cf9733c203f3e45da0089a6381248f2
YZ318 <YakovZhuravlev88@gmail.com>
2023-04-22 13:49:15 +0300

    Задача 13

---
+++ home13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+
+if (number < 100 || number > 999)
+{
+    Console.WriteLine("Третьей цифры нет.");
+}
+else
+{
+    int thirdDigit = (number % 100) % 10;
+    Console.WriteLine("Третья цифра введенного числа: " + thirdDigit);
+}
