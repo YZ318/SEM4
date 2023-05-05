7e6436f7d9b3aea4aa00bdf0f4231f6d4a27c0a8
YZ318 <YakovZhuravlev88@gmail.com>
2023-05-05 12:03:14 +0300

    Сделал задачу 25

---
+++ HomeW25/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число A: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите степень B: ");
+int b = int.Parse(Console.ReadLine());
+
+int result = 1;
+
+for (int i = 1; i <= b; i++)
+{
+    result *= a;
+}
+
+Console.WriteLine("{0} в степени {1} равно {2}", a, b, result);
